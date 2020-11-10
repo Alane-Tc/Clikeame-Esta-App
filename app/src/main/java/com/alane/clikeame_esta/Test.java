a687c56aa6da28ebaff2eba919e1f4e387493ae1
Alane-Tc <alantamez28@hotmail.com>
2020-11-10 16:34:33 -0600

    New images and modified files

--- app/src/main/java/com/alane/clikeame_esta/Test.java
+++ app/src/main/java/com/alane/clikeame_esta/Test.java
@@ -3,10 +3,12 @@ package com.alane.clikeame_esta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +17,11 @@ public class Test extends AppCompatActivity {
     String[] answer = {"Ya perdiste jajaja", "Sigue intentado", "Qué pendejo estas", "Vulve a intentarlo", "JAJAJAJAJA",
             "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"};
 
+    Integer[] imagenesID = {R.drawable.win1, R.drawable.win2, R.drawable.win3,R.drawable.win4,R.drawable.win5,
+            R.drawable.win6,R.drawable.win7,R.drawable.win8,R.drawable.win9,R.drawable.win10};
+
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         Button boton_counter = (Button) findViewById(R.id.btn_counter_test);
@@ -38,11 +44,13 @@ public class Test extends AppCompatActivity {
         if (valorDado == 5){
             buttonColor();
             texto_random.setText(answer[(int) Math.floor(Math.random()*answer.length)]);
+
         }
 
         if (counter ==50){
             buttonColor();
-            texto_random.setText("Felicidades crack");
+            showImgRandom();
+            Toast.makeText(this, "Felicidades Ganaste el juego", Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -51,5 +59,10 @@ public class Test extends AppCompatActivity {
         boton_counter.setEnabled(false);
         boton_counter.setTextColor(Color.parseColor("#22223b"));
         boton_counter.setBackgroundColor(Color.parseColor("#8d99ae"));
+    }
+
+    private void showImgRandom(){
+        ImageView imgRandom = (ImageView) findViewById(R.id.imgRandomEasy);
+        imgRandom.setImageResource(imagenesID[(int)Math.floor(Math.random()*imagenesID.length)]);
     }
 }
