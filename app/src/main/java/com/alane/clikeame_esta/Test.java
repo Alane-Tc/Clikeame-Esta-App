61a80f44d5c5fab54c3548e529640f33e16566da
Alane-Tc <alantamez28@hotmail.com>
2020-11-10 14:55:15 -0600

    delete class and modified class test

--- app/src/main/java/com/alane/clikeame_esta/Test.java
+++ app/src/main/java/com/alane/clikeame_esta/Test.java
@@ -3,6 +3,7 @@ package com.alane.clikeame_esta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,33 +16,40 @@ public class Test extends AppCompatActivity {
             "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"};
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         Button boton_counter = (Button) findViewById(R.id.btn_counter_test);
         boton_counter.setOnClickListener(this::onClick);
     }
 
-
     @SuppressLint("SetTextI18n")
     public void onClick(View view){
         counter ++;
         int valorDado = (int) Math.floor(Math.random()*20+1);
         int number = (int) Math.floor(Math.random()*answer.length);
-        if (counter == 1){
-            Toast.makeText(Test.this, "Iniciaste el juego", Toast.LENGTH_LONG).show();
-        }
-
-
 
         TextView texto_Counter = (TextView) findViewById(R.id.show_clicks_test);
         TextView texto_random = (TextView) findViewById(R.id.textRandom);
         texto_Counter.setText("Has Dado Click: "+counter+ " Veces");
-
-        if (valorDado == 5){
-            texto_random.setText(answer[(int) Math.floor(Math.random()*answer.length)]);
-
+        if (counter == 1){
+            texto_random.setText("Iniciaste el juego");
         }
 
+        if (valorDado == 5){
+            buttonColor();
+            texto_random.setText(answer[(int) Math.floor(Math.random()*answer.length)]);
+        }
+
+        if (counter ==50){
+            buttonColor();
+            texto_random.setText("Felicidades crack");
+        }
+
+    }
+    private void buttonColor(){
+        Button boton_counter = (Button) findViewById(R.id.btn_counter_test);
+        boton_counter.setEnabled(false);
+        boton_counter.setTextColor(Color.parseColor("#22223b"));
+        boton_counter.setBackgroundColor(Color.parseColor("#8d99ae"));
     }
 }
