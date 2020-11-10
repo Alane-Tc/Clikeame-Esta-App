b43f0c4822b325ce3c733ec6716663fdff70f139
Alane-Tc <alantamez28@hotmail.com>
2020-11-10 17:22:13 -0600

    update class

---
+++ app/src/main/java/com/alane/clikeame_esta/Difficult.java
@@ -0,0 +1,14 @@
+package com.alane.clikeame_esta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Difficult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficult);
+    }
+}
